6e017408b696da4b89bcbd6c05a807f0f84b1ddf
Vu Quang Dat <vuquangdat77@gmail.com>
2023-11-12 09:15:18 +0700

    Move,rotation,jump

--- Assets/MyGame/Scripts/PlayerMoveTest.cs
+++ Assets/MyGame/Scripts/PlayerMoveTest.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class PlayerMoveTest : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float turnSpeed;
+    [SerializeField]
+    private float jumfore;
+    private float horizontalInput;
+    private float verticalInput;
+
+    private float yfore;
+
+
+    private CharacterController characterController;
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 movementDirection = new Vector3 (horizontalInput,0, verticalInput);
+        print($"Vector Magnitude before normalize: {movementDirection.magnitude}");
+
+        float magnitude = movementDirection.magnitude;
+        magnitude = Mathf.Clamp01( magnitude );
+
+        movementDirection.Normalize();
+        print($"Vector Magnitude after normalize: {movementDirection.magnitude}");
+
+        yfore = Physics.gravity.y-Time.deltaTime;
+
+        if(characterController.isGrounded)
+        {
+            yfore = 0;
+            if(Input.GetButtonDown("Jump"))
+            {
+                yfore = jumfore;
+            }
+        }
+
+        Vector3 velocity = moveSpeed * movementDirection * magnitude;
+        velocity.y = yfore;
+
+        characterController.Move(velocity*Time.deltaTime);
+        if(movementDirection != Vector3.zero )
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection,Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, turnSpeed*Time.deltaTime);
+        }
     }
 }
